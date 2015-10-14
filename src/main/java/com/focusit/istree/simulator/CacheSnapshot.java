6f127709eab218ac55173125db2694ec8b731fe4
Denis Kirpichenkov <dkirpichenkov@naumen.ru>
2015-10-14 23:09:32 +0500

    q

---
+++ src/main/java/com/focusit/istree/simulator/CacheSnapshot.java
@@ -0,0 +1,86 @@
+package com.focusit.istree.simulator;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.Lock;
+
+import com.google.common.util.concurrent.Striped;
+
+public class CacheSnapshot<K, V>
+{
+    public static interface Loader<K, V>
+    {
+        V load(K key);
+    }
+
+    private final static Object NO_OBJECT = new Object();
+    private final static Striped<Lock> locks = Striped.lock(32);
+
+    private final AtomicReference<Loader<K, V>> loaderRef = new AtomicReference<>();
+
+    private final AtomicReference<ConcurrentMap<K, Object>> storageRef = new AtomicReference<>(
+            new ConcurrentHashMap<>());
+
+    public void build()
+    {
+
+    }
+
+    @SuppressWarnings("unchecked")
+    public V get(K key)
+    {
+        ConcurrentMap<K, Object> data = storageRef.get();
+
+        Object result = data.get(key);
+        if (result == null)
+        {
+            Loader<K, V> loader = loaderRef.get();
+            if (loader != null)
+            {
+                result = putIfMissedInternal(key);
+            }
+        }
+
+        if (result == NO_OBJECT)
+        {
+            result = null;
+        }
+
+        return (V)result;
+    }
+
+    public final void reset()
+    {
+        storageRef.set(new ConcurrentHashMap<>());
+    }
+
+    public void setLoader(Loader<K, V> loader)
+    {
+        this.loaderRef.set(loader);
+    }
+
+    private final Object putIfMissedInternal(K key)
+    {
+        Lock lock = locks.get(key);
+        lock.lock();
+        try
+        {
+            Loader<K, V> loader = loaderRef.get();
+            ConcurrentMap<K, Object> data = storageRef.get();
+            Object value = loader.load(key);
+
+            if (value == null)
+            {
+                value = NO_OBJECT;
+            }
+
+            data.put(key, value);
+            return value;
+        }
+        finally
+        {
+            lock.unlock();
+        }
+    }
+}
---
+++ src/main/java/com/focusit/istree/simulator/Pair.java
@@ -0,0 +1,38 @@
+package com.focusit.istree.simulator;
+
+import com.google.common.base.Objects;
+
+public class Pair<F, S>
+{
+    public static <A, B> Pair<A, B> create(A a, B b)
+    {
+        return new Pair<A, B>(a, b);
+    }
+
+    private F first;
+
+    private S second;
+
+    public Pair(F first, S second)
+    {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (!(o instanceof Pair))
+        {
+            return false;
+        }
+        Pair<?, ?> p = (Pair<?, ?>)o;
+        return Objects.equal(p.first, first) && Objects.equal(p.second, second);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return (first == null ? 0 : first.hashCode()) ^ (second == null ? 0 : second.hashCode());
+    }
+}
