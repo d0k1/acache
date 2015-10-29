e867e2ab910866775a44bda4f51101d4291c4ccd
dOkI <denis.kirpichenkov@gmail.com>
2015-10-30 02:41:34 +0500

    lazy cache for snapshots

---
+++ src/main/java/com/focusit/istree/benchmarks/hotcache/LSCTest01.java
@@ -0,0 +1,54 @@
+package com.focusit.istree.benchmarks.hotcache;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+
+import com.focusit.istree.hotcache.LazySnapshotCache;
+import com.focusit.istree.hotcache.LazySnapshotCache.Loader;
+
+@Threads( 100 )
+public class LSCTest01 {
+    @State(Scope.Benchmark)
+    public static class LSCState
+    {
+    	LazySnapshotCache<String, String> cache = new LazySnapshotCache<>();
+    	public int position = 0;
+    	public LSCState() {
+    		cache.setLoader(new Loader<String, String>() {
+
+				@Override
+				public String load(String key) {
+					Blackhole.consumeCPU(20000);
+					return "V"+key;
+				}
+			});
+		}
+    }
+    
+    @Benchmark
+    @BenchmarkMode(value = { Mode.AverageTime })
+    @Warmup(iterations = 5, time=10, timeUnit=TimeUnit.SECONDS)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @Measurement(iterations = 5, time=20, timeUnit=TimeUnit.SECONDS)
+    @Fork(value = 2)
+    public int testPutALot(LSCState state)
+    {
+    	int count = 100;
+    	for(int i=0;i<count;i++){
+    		state.cache.get(""+state.position+i);
+    	}
+    	state.position+=count;
+    	return 0;
+    }
+}
---
+++ src/main/java/com/focusit/istree/hotcache/LazySnapshotCache.java
@@ -0,0 +1,73 @@
+package com.focusit.istree.hotcache;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.Lock;
+
+import com.google.common.util.concurrent.Striped;
+
+public class LazySnapshotCache<K, V> {
+	public static interface Loader<K, V> {
+		V load(K key);
+	}
+
+	private final static Object NO_OBJECT = new Object();
+	private final static Striped<Lock> locks = Striped.lock(32);
+
+	private final AtomicReference<Loader<K, V>> loaderRef = new AtomicReference<>();
+
+	private final AtomicReference<ConcurrentMap<K, Object>> storageRef = new AtomicReference<>(
+			new ConcurrentHashMap<>());
+
+	public void build() {
+
+	}
+
+	@SuppressWarnings("unchecked")
+	public V get(K key) {
+		ConcurrentMap<K, Object> data = storageRef.get();
+
+		Object result = data.get(key);
+		if (result == null) {
+			Lock lock = locks.get(key);
+			lock.lock();
+			try {
+				result = data.get(key);
+				if (result == null) {
+					Loader<K, V> loader = loaderRef.get();
+					if (loader != null) {
+						result = putIfMissedInternal(data, loader, key);
+					}
+				}
+			} finally {
+				lock.unlock();
+			}
+		}
+
+		if (result == NO_OBJECT) {
+			result = null;
+		}
+
+		return (V) result;
+	}
+
+	public final void reset() {
+		storageRef.set(new ConcurrentHashMap<>());
+	}
+
+	public void setLoader(Loader<K, V> loader) {
+		this.loaderRef.set(loader);
+	}
+
+	private final Object putIfMissedInternal(ConcurrentMap<K, Object> data, Loader<K,V> loader, K key) {
+		Object value = loader.load(key);
+
+		if (value == null) {
+			value = NO_OBJECT;
+		}
+
+		data.put(key, value);
+		return value;
+	}
+}
